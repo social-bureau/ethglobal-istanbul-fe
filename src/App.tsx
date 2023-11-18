62d1b0ae92b25f9599e4e6f97d91c3a2471bf210
Ikhalas Mannoon <ikhalas.work@gmail.com>
2023-11-18 12:34:13 +0300

    setup web3modal provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import AppRoutes from "./route/AppRoutes";
 import { CombineComponents } from "./wrapper/CombineComponents";
 import FlowbiteProvider from "./wrapper/FlowbiteProvider";
 import ReduxProvider from "./wrapper/ReduxProvider";
+import WagmiProvider from "./wrapper/WagmiProvider";
 
-const providers = [ReduxProvider, FlowbiteProvider, BrowserRouter];
+const providers = [
+  ReduxProvider,
+  FlowbiteProvider,
+  WagmiProvider,
+  BrowserRouter,
+];
 const AppProvider = CombineComponents(...providers);
 
 export default function App() {
